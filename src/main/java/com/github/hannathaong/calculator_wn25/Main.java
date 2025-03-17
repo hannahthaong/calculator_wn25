176d5f32a6c84d41dda27b15c53f5b6b2efc2520
Hannah Nguyen <hannahhw.anh@gmail.com>
2025-03-17 15:17:07 -0700

    Add initial Calculator code

--- src/main/java/com/github/hannathaong/calculator_wn25/Main.java
+++ src/main/java/com/github/hannathaong/calculator_wn25/Main.java
@@ -3,7 +3,6 @@ package com.github.hannathaong.calculator_wn25;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-
         int sum = calculator.add(5, 3);
         System.out.println("5 + 3 = " + sum);
     }
